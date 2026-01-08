e198b40d8bfc00dc23ab14cb207667bf561d2977
Varshith1621 <amrvarshith1612@gmail.com>
2026-01-08 12:14:13 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,8 @@
+class Variables{
+    public static void main(String args[]){
+        int num1 = 4;
+        int num2 = 7;
+        int result = num1 + num2;
+        System.out.println("The sum is: " + result);
+    }
+}
