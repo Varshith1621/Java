74abca362ee3fde87f196b9a9c55bcdb54722194
Varshith1621 <amrvarshith1612@gmail.com>
2026-01-09 22:59:02 +0530

    DataType

---
+++ DataType.java
@@ -0,0 +1,19 @@
+class DataType{
+    public static void main(String args[]){
+        // Integer data type
+        int num = 11; // 4 bytes
+        byte by = 127; // 1 byte
+        short sh = 558; // 2 bytes
+        long l = 5854l; // 8 bytes
+
+        // Floating point data type
+        float f = 5.6f; // 4 bytes
+        double d = 4.589; // 8 bytes
+        
+        // Character data type
+        char c = 'P'; // 2 bytes
+
+        // Boolean data type
+        boolean b = true; // 1 bit
+    }
+}
