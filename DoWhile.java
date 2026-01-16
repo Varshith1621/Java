d38b8658e00a5b57c89f4b91e461bbca522a170e
Varshith <amrvarshith1612@gmail.com>
2026-01-16 21:18:37 +0530

    Do While Loop

---
+++ DoWhile.java
@@ -0,0 +1,10 @@
+class DoWhile {
+    public static void main(String[] args) {
+        int i = 5;
+
+        do {
+            System.out.println("Hi there!" + i);
+            i++;
+        } while (i <= 4);
+    }
+}
