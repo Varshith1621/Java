96c6c813d341c2554e44e1e261c35288a558154f
Varshith <amrvarshith1612@gmail.com>
2026-01-22 19:19:46 +0530

    String in Java

---
+++ String_in_java.java
@@ -0,0 +1,10 @@
+public class String_in_java {
+    public static void main(String[] args) {
+        String name =  "Varshith";//new String ("Varshith");
+        System.err.println(name);
+        System.err.println(name.hashCode());
+        System.err.println("Priya & " + name);
+        System.out.println(name.charAt(5));
+        //System.out.println(name.concat(" & Priya"));
+    }
+}
