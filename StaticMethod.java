e4ba4c108f739ef0ba050af00a540480159a167b
Varshith <amrvarshith1612@gmail.com>
2026-01-24 11:00:39 +0530

    Static Method in Java

---
+++ StaticMethod.java
@@ -0,0 +1,41 @@
+class Mobile {
+    String brand;
+    int price;
+    static  String network;
+    String name;
+
+    public void show(){
+        System.err.println("Using static Variable:");
+        System.err.println("Brand:" + brand + " Price:" + price + " Network:" + network + " Name:" + name);
+    }
+
+    public static void show1(Mobile obj){ //static method
+        System.err.println("Brand:" + obj.brand + " Price:" + obj.price + " Network:" + network + " Name:" + obj.name);
+    }
+}
+
+public class StaticMethod {
+    public static void main(String[] args) {
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Apple";
+        obj1.price = 1500;
+        obj1.name = "iPhone 16 Pro";
+        Mobile.network = "5G"; //static variable should be accessed by class name
+
+        Mobile obj2 = new Mobile(); //static variable shared among all instances
+        obj2.brand = "Samsung";
+        obj2.price = 1200;
+        obj2.name = "Galaxy S23 Ultra";
+        Mobile.network = "5G";
+
+        Mobile.network = "6G"; //static variable changed reflected in all objects
+
+        obj1.show();
+        obj2.show();
+
+        System.err.println("Using static method:");
+        Mobile.show1(obj1); //static method should be accessed by class name 
+        //static method can access static variable directly and non-static variable by object reference
+        Mobile.show1(obj2);
+    }
+    }
