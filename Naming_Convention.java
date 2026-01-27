4e5d4fbed33da02e9eb76667b82de3ce12f950e9
Varshith <amrvarshith1612@gmail.com>
2026-01-27 17:15:15 +0530

    Naming Convention in java

---
+++ Naming_Convention.java
@@ -0,0 +1,21 @@
+public class Naming_Convention {
+    public static void main(String[] args) {
+        
+    }
+}
+// Camel Case: variableName, methodName, className
+// Pascal Case: ClassName, InterfaceName
+// Snake Case: variable_name, method_name
+
+// class names should start with Uppercase letter
+// Interface names should start with Uppercase letter
+// variable and method names should start with lowercase letter
+// Constants should be in ALL UPPERCASE letters with words separated by underscores
+// Package names should be in all lowercase letters
+//Conctructor names should match the class name exactly
+
+//Conconation: use camelCase for multi-word names (e.g., myVariableName)
+// Avoid using special characters and spaces in names
+// Use meaningful and descriptive names
+// Avoid using reserved keywords as names
+// Be consistent with naming conventions throughout the codebase
