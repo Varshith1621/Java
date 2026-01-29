2d050ff77809185c473058db8d2eefb6dee8b007
Varshith <amrvarshith1612@gmail.com>
2026-01-29 22:29:13 +0530

    Package.java

---
+++ Packages_in_java/package.java
@@ -0,0 +1,15 @@
+import tools.Calc;
+import tools.AdvCalc;
+import java.util.ArrayList;
+import java.lang.*; // By Default this package is installed in every class file
+
+public class package{
+  public static void main(String args[]){
+    Arraylist list = new ArrayList();
+
+   Calc obj = new Calc();
+    AdvCalc obj1 = new AdvCalc();
+
+    System.out.println();
+  }
+}
